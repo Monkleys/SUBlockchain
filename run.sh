73717e4c6a77a167b8c00f73e976c7ddcad6a6f4
Reece <monkleys@gmail.com>
2018-10-17 19:42:39 +0200

    Update readme

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-sudo chmod -R 777 here/
 sudo apt install ntpdate
 sudo ntpdate ntp.sun.ac.za
-docker build --network=host -t ethtest .
-docker run --network=host -it -v "${PWD}/here":"/blkchain1" -e TZ=Africa/Johannesburg ethtest
+#docker build --network=host -t ethtest .
+sudo docker pull monkleys/ethtest
+sudo docker run --network=host -it -v "${PWD}/here":"/blkchain1" -e TZ=Africa/Johannesburg ethtest
 
